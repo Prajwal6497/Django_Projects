95a536b0960aa19d0a61223adb0b379eb33e783c
Prajwal6497 <prajwalprajwal7946@gmail.com>
2023-08-20 23:30:08 +0530

    added code

--- django_Ajax_CRUD/django_Ajax_CRUD/urls.py
+++ django_Ajax_CRUD/django_Ajax_CRUD/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('save/', views.save_data, name="save"),
     path('delete/', views.delete_data, name="delete"),
+    path('edit/', views.edit_data, name="edit"),
 ]
--- django_Ajax_CRUD/enroll/views.py
+++ django_Ajax_CRUD/enroll/views.py
@@ -16,10 +16,14 @@ def save_data(request):
     if request.method == "POST":
         form = StudentUserRegistration(request.POST)
         if form.is_valid():
+            sid = request.POST.get('stuid')
             name = request.POST["name"]
             email = request.POST["email"]
             password = request.POST["password"]
-            usr = User(name = name, email = email, password = password)
+            if (sid == ''):
+                usr = User(name = name, email = email, password = password)
+            else:
+                usr = User(id=sid, name=name, email=email, password=password)
             usr.save()
             stud = User.objects.values()
             student_data = list(stud)
@@ -37,3 +41,11 @@ def delete_data(request):
         return JsonResponse({'status': 1})
     else:
         return JsonResponse({'status': 0})
+    
+def edit_data(request):
+    if request.method == "POST":
+        id = request.POST.get('sid')
+        print(id)
+        student = User.objects.get(pk=id)
+        student_data = {"id": student.id, "name": student.name, "email": student.email, "password": student.password}
+        return JsonResponse(student_data)
